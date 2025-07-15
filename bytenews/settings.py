42e1de78d1e670aac6b9e45d947772f2307e5478
rongala pravalika <rongalapravalika2006@gmail.com>
2025-07-15 22:28:42 +0530

    Updated exixting files

--- bytenews/settings.py
+++ bytenews/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'bytenews.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +68,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'bytenews.wsgi.application'
 
@@ -121,3 +124,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+# Redirect after login
+LOGIN_REDIRECT_URL = '/'
+
+# Redirect after logout (optional)
+LOGOUT_REDIRECT_URL = '/login/'
+
+# Tells Django where the login page is (useful if login_required is used)
+LOGIN_URL = '/login/'
+
+LOGOUT_REDIRECT_URL = '/logged_out/'
+
+
--- bytenews/urls.py
+++ bytenews/urls.py
@@ -15,8 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from users import views as user_views
+from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('register/', user_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name='logout'),
+    path('', include('news.urls')),  # Your news app home
+    path('', include('users.urls')),  # includes login/logout
+    path('logged_out/', TemplateView.as_view(template_name='registration/logged_out.html')),
 ]
+

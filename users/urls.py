6f84b51abafac1b5255d3f86cf6ea739d9856615
rongala pravalika <rongalapravalika2006@gmail.com>
2025-07-15 22:37:09 +0530

    Updated existing files

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/logged_out/'), name='logout'),
-
 ]
